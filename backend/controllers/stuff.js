899965c8372b13cdaf2e67bdcacfdcee7d64844a
Ludovic <ludovic.mangaj@gmail.com>
2022-04-04 16:03:47 +0200

    the file downloading modification works

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -43,8 +43,13 @@ exports.deleteOneThing = (req, res, next) => {
         })
 }
 
-exports.modifiyOneThing = (req, res, next) => {
-    Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id })
-        .then(() => res.status(204).json({message : "hey"}))
-        .catch(error => res.status(400).json({ error }))
-}
+exports.modifyOneThing = (req, res, next) => {
+    const thingObject = req.file ?
+      {
+        ...JSON.parse(req.body.thing),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      } : { ...req.body };
+    Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  };
